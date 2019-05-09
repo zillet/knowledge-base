0732f3f4b81617c3ce7d31b88b90b5e5f90525ee
man15h <manish9461@gmail.com>
2019-05-10 00:24:25 +0530

    Category broken links

--- src/components/home/HomeCategories.vue
+++ src/components/home/HomeCategories.vue
@@ -1,50 +1,56 @@
 <template>
   <div class="category__wrapper">
-    <Card
-      center
-      class="category">
-      <g-image
-        src="@/assets/illustrations/getting_started.svg"
-        width="180" />
-      <div class="category__content">
-        <h4>
-          Get Started
-        </h4>
-        <p>
-          Step by step instructions on how to use Zillet & all its features
-        </p>
-      </div>
-    </Card>
-    <Card
-      center
-      class="category">
-      <g-image
-        src="@/assets/illustrations/troubleshooting.svg"
-        width="150" />
-      <div class="category__content">
-        <h4>
-          Troubleshooting issues
-        </h4>
-        <p>
-          Stuck? Get help solving a problem or the error message.
-        </p>
-      </div>
-    </Card>
-    <Card
-      center
-      class="category">
-      <g-image
-        src="@/assets/illustrations/security.svg"
-        width="180" />
-      <div class="category__content">
-        <h4>
-          Security
-        </h4>
-        <p>
-          Learn how to protect yourself and your cryptocurrency.
-        </p>
-      </div>
-    </Card>
+    <g-link to="/getting-started">
+      <Card
+        center
+        class="category">
+        <g-image
+          src="@/assets/illustrations/getting_started.svg"
+          width="180" />
+        <div class="category__content">
+          <h4>
+            Get Started
+          </h4>
+          <p>
+            Step by step instructions on how to use Zillet & all its features
+          </p>
+        </div>
+      </Card>
+    </g-link>
+    <g-link to="/troubleshooting">
+      <Card
+        center
+        class="category">
+        <g-image
+          src="@/assets/illustrations/troubleshooting.svg"
+          width="150" />
+        <div class="category__content">
+          <h4>
+            Troubleshooting issues
+          </h4>
+          <p>
+            Stuck? Get help solving a problem or the error message.
+          </p>
+        </div>
+      </Card>
+    </g-link>
+    <g-link to="/security">
+      <Card
+        center
+        class="category">
+        <g-image
+          src="@/assets/illustrations/security.svg"
+          width="180" />
+        <div class="category__content">
+          <h4>
+            Security
+          </h4>
+          <p>
+            Learn how to protect yourself and your cryptocurrency.
+          </p>
+        </div>
+      </Card>
+    </g-link>
   </div>
 </template>
 <style lang="scss" scoped>
---
+++ src/pages/GettingStarted.vue
@@ -0,0 +1,23 @@
+<template>
+  <Home>
+    {{ $page.article }}
+  </Home>
+</template>
+<page-query>
+query GettingStarted {
+  article: allGettingStarted{
+    edges{
+      node{
+        title
+        category
+				excerpt
+        path
+        timeToRead
+        fileInfo{
+          directory
+        }
+      }
+    }
+  }
+}
+</page-query>
