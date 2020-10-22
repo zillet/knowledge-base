4819bc074c916dced2b1948328ca19d79afe2c1e
man15h <manish9461@gmail.com>
2020-10-23 01:23:22 +0530

    minor fix

--- src/components/home/HomePopular.vue
+++ src/components/home/HomePopular.vue
@@ -2,6 +2,7 @@
   <Section>
     <h3> Popular Articles <i class="eva eva-arrow-downward-outline" /> </h3>
     <List :articles="articles" />
+    {{ articles }}
   </Section>
 </template>
 <script>
@@ -16,10 +17,10 @@ export default {
         {
           node: {
             title: 'Staking on Zillet',
-            path: 'staking-on-zillet',
-            excerpt: `  When you stake your ZILs, your receive ZIL and gZIL (Governance ZIL) as a reward, this means you are allowed to vote
-        on governance proposals on the Zilliqa ecosystem. But the best `,
-            date: new Date()
+            path: '/staking-on-zillet',
+            excerpt: `When you stake your ZILs, your receive ZIL and gZIL (Governance ZIL) as a reward, this means you are allowed to vote on governance proposals on the Zilliqa ecosystem. But the best `,
+            date: '2020-10-23',
+            priority: 100
           }
         },
         ...this.$page.gettingStartedArticles.edges,
